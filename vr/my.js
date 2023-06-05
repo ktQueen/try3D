a66592efff1b5af41bd2f9a5e8a47bc6272f04b7
kangitng1 <kangting1@xiaomi.com>
2023-06-05 17:12:36 +0800

    feat:正方体

---
+++ vr/my.js
@@ -0,0 +1,7 @@
+/*
+ * @Description:
+ * @Date: 2023-06-02 20:23:48
+ * @LastEditTime: 2023-06-05 09:35:25
+ */
+
+import * as THREE from "three";
--- vr/src/views/Home.vue
+++ vr/src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="container">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div class="container" ref="container"></div>
 </template>
 
 <script lang="ts" setup>
 import * as THREE from "three";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-import a from "@types/three.d.ts";
+import { ref, onMounted } from "vue";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // 初始化场景
 const scene = new THREE.Scene();
 // 初始化相机
-const camera = new THREE.per();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerHeight / window.innerWidth,
+  0.1,
+  1000
+);
+// 设置相机位置
+camera.position.z = 5;
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer();
+// 设置渲染器尺寸
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const container = ref(null);
+
+const render = () => {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+};
+
+// 添加立方体
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// 添加球体
+var arr = ["4_b", "4_f", "4_l", "4_r", "4-u", "4-d"];
+
+// 挂载完毕之后获取dom
+onMounted(() => {
+  // 添加控制器
+  const controls = new OrbitControls(camera, container.value);
+  controls.enableDamping = true;
+  container.value.appendChild(renderer.domElement);
+  render();
+});
 </script>
 <style lang="scss" scoped>
 .container {
